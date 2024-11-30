d9d59c7e31e4ef23a986296748ebc589ef182bd1
Jarvis Lee <John0988721002@gmail.com>
2024-11-30 18:43:51 +0800

    first commit

---
+++ src/main/java/com/smashspot/member/model/MemberVO.java
@@ -0,0 +1,24 @@
+package com.smashspot.member.model;
+
+public class MemberVO {
+	private Integer memId;
+	private String memName;
+
+	public Integer getMemId() {
+		return memId;
+	}
+
+	public void setMemId(Integer memId) {
+		this.memId = memId;
+	}
+
+	public String getMemName() {
+		return memName;
+	}
+
+	public void setMemName(String memName) {
+		this.memName = memName;
+	}
+	
+
+}
