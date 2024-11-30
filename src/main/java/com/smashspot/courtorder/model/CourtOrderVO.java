8f08701cf7c427e13bd1e7e8c6656a55e9b23c50
Jimmy Long <yoyo7916686@gmail.com>
2024-11-30 19:39:54 +0800

    FirstCommit

---
+++ src/main/java/com/smashspot/courtorder/model/CourtOrderVO.java
@@ -0,0 +1,13 @@
+package com.smashspot.courtorder.model;
+
+public class CourtOrderVO {
+	private Boolean OrderStatus;
+
+	public Boolean getOrderStatus() {
+		return OrderStatus;
+	}
+
+	public void setOrderStatus(Boolean orderStatus) {
+		OrderStatus = orderStatus;
+	}
+}
