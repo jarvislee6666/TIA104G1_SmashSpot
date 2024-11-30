cb4503e21aeb91405827f54c224c917c98657cb3
jarvislee6666 <John0988721002@gmail.com>
2024-11-30 21:41:14 +0800

    Revert "SecondCommit"

--- src/main/java/com/smashspot/courtorder/model/CourtOrderVO.java
+++ src/main/java/com/smashspot/courtorder/model/CourtOrderVO.java
@@ -3,4 +3,11 @@ package com.smashspot.courtorder.model;
 public class CourtOrderVO {
 	private Boolean OrderStatus;
 
+	public Boolean getOrderStatus() {
+		return OrderStatus;
+	}
+
+	public void setOrderStatus(Boolean orderStatus) {
+		OrderStatus = orderStatus;
+	}
 }
