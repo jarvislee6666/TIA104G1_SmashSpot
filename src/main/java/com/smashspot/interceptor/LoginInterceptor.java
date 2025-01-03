4605ae6c496b8683eff5b5c2e23dbfe0f878737c
Jarvis Lee <John0988721002@gmail.com>
2025-01-03 21:10:02 +0800

    update LoginInterceptor and MemberController

--- src/main/java/com/smashspot/interceptor/LoginInterceptor.java
+++ src/main/java/com/smashspot/interceptor/LoginInterceptor.java
@@ -9,13 +9,17 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        HttpSession session = request.getSession();
-        if (session.getAttribute("login") == null) {
-            response.sendRedirect("/member/login");
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+	    HttpSession session = request.getSession();
+	    if (session.getAttribute("login") == null) {
+	        String requestUri = request.getRequestURI();
+	        String queryString = request.getQueryString();
+	        String redirectUrl = queryString != null ? requestUri + "?" + queryString : requestUri;
+	        session.setAttribute("redirectUrl", redirectUrl);
+	        response.sendRedirect(request.getContextPath() + "/member/login");
+	        return false;
+	    }
+	    return true;
+	}
 }
