594c4b7194047db67cf1608246786ef0e98643e6
Jarvis Lee <John0988721002@gmail.com>
2025-01-07 02:42:18 +0800

    add listAllChart.html and update CourtOrder、ReservationTime Model

--- src/main/java/com/smashspot/reservationtime/model/ReservationTimeRepository.java
+++ src/main/java/com/smashspot/reservationtime/model/ReservationTimeRepository.java
@@ -77,4 +77,15 @@ public interface ReservationTimeRepository extends JpaRepository<ReservationTime
    List<ReservationTimeVO> findHolidayByStadiumIdAndDatesGreaterThanEqual(
                    @Param("stdmId") Integer stdmId,
                    @Param("dates") Date dates);
+   
+   //沃寯添加
+   @Query("SELECT r FROM ReservationTimeVO r " +
+	       "WHERE r.stadium.stdmId = :stdmId " +
+	       "AND r.dates BETWEEN :startDate AND :endDate")
+	List<ReservationTimeVO> findByStadiumIdAndDatesBetween(
+	    @Param("stdmId") Integer stdmId,
+	    @Param("startDate") Date startDate,
+	    @Param("endDate") Date endDate
+	);
+   
 }
