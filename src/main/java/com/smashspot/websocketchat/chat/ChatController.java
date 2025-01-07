ffb92145eec614daa0e7dd76d7698aa35ea126e0
Belle Lee <belle970062@gmail.com>
2025-01-07 15:20:47 +0800

    conflict

--- src/main/java/com/smashspot/websocketchat/chat/ChatController.java
+++ src/main/java/com/smashspot/websocketchat/chat/ChatController.java
@@ -1,44 +1,44 @@
-package com.smashspot.websocketchat.chat;
-
-import java.util.List;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-import lombok.RequiredArgsConstructor;
-
-@Controller  // 改用 @RestController 因為要返回 JSON 數據
-@RequiredArgsConstructor
-public class ChatController {
-    
-    private final SimpMessagingTemplate messagingTemplate;
-	private final ChatMessageService chatMessageService;
-
-    @MessageMapping("/chat")
-    public void processMessage(@Payload ChatMessage chatMessage) {
-    	ChatMessage saveMsg = chatMessageService.save(chatMessage);
-    	// bella/queue/messages
-    	messagingTemplate.convertAndSendToUser(
-    			chatMessage.getRecipientId(),
-    			"/queue/messages",
-    			ChatNotification.builder()
-    			.id(saveMsg.getId())
-    			.senderId(saveMsg.getSenderId())
-    			.recipientId(saveMsg.getRecipientId())
-    			.content(saveMsg.getContent())
-    			.build()  			
-    			);
-    }
-    
-    @GetMapping("/messages/{senderId}/{recipientId}")
-    public ResponseEntity<List<ChatMessage>> findChatMessages(
-            @PathVariable String senderId,
-            @PathVariable String recipientId) {
-        return ResponseEntity.ok(chatMessageService.findChatMessages(senderId, recipientId));
-    }
-}
+//package com.smashspot.websocketchat.chat;
+//
+//import java.util.List;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.handler.annotation.Payload;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//import lombok.RequiredArgsConstructor;
+//
+//@Controller  // 改用 @RestController 因為要返回 JSON 數據
+//@RequiredArgsConstructor
+//public class ChatController {
+//    
+//    private final SimpMessagingTemplate messagingTemplate;
+//	private final ChatMessageService chatMessageService;
+//
+//    @MessageMapping("/chat")
+//    public void processMessage(@Payload ChatMessage chatMessage) {
+//    	ChatMessage saveMsg = chatMessageService.save(chatMessage);
+//    	// bella/queue/messages
+//    	messagingTemplate.convertAndSendToUser(
+//    			chatMessage.getRecipientId(),
+//    			"/queue/messages",
+//    			ChatNotification.builder()
+//    			.id(saveMsg.getId())
+//    			.senderId(saveMsg.getSenderId())
+//    			.recipientId(saveMsg.getRecipientId())
+//    			.content(saveMsg.getContent())
+//    			.build()  			
+//    			);
+//    }
+//    
+//    @GetMapping("/messages/{senderId}/{recipientId}")
+//    public ResponseEntity<List<ChatMessage>> findChatMessages(
+//            @PathVariable String senderId,
+//            @PathVariable String recipientId) {
+//        return ResponseEntity.ok(chatMessageService.findChatMessages(senderId, recipientId));
+//    }
+//}
