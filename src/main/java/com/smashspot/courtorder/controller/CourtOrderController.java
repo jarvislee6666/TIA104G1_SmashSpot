9877d2d2ab74fa4ae62cee9ab9188d04163b898b
Jimmy Long <yoyo7916686@gmail.com>
2025-01-07 13:56:10 +0800

    update CourtOrder

--- src/main/java/com/smashspot/courtorder/controller/CourtOrderController.java
+++ src/main/java/com/smashspot/courtorder/controller/CourtOrderController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -112,9 +113,5 @@ public class CourtOrderController {
         // 直接回傳 CourtOrderVO，其中有 Set<CourtOrderDetailVO>
         return list;
     }
-	
-	
-
-
 
 }
