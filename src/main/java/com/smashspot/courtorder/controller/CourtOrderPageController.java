9301ae383c60da4e8bbbd19c71dba5bbb794d631
Jimmy Long <yoyo7916686@gmail.com>
2025-01-07 20:06:43 +0800

    update appointment-records

--- src/main/java/com/smashspot/courtorder/controller/CourtOrderPageController.java
+++ src/main/java/com/smashspot/courtorder/controller/CourtOrderPageController.java
@@ -2,6 +2,8 @@ package com.smashspot.courtorder.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,24 +12,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smashspot.courtorder.model.CourtOrderService;
 import com.smashspot.courtorder.model.CourtOrderVO;
+import com.smashspot.member.model.MemberVO;
 
 @Controller
-@RequestMapping("/court-order")
+@RequestMapping("/member")
 public class CourtOrderPageController {
 
     @Autowired
     private CourtOrderService courtOrderSvc;
 
-    @GetMapping("/appointmentRecord")
-    public String showAppointmentRecord(Model model) {
+    @GetMapping("/appointment-records")
+    public String showAppointmentRecord(HttpSession session, Model model) {
+        // 1) 從 Session 取出目前登入的會員物件
+        MemberVO loginUser = (MemberVO) session.getAttribute("login");
+        if (loginUser == null) {
+            // 尚未登入 -> 你可以選擇導回登入頁
+            return "redirect:/member/login"; 
+        }
+        
+        // 2) 拿會員ID (假設 MemberVO 有一個 getMemid() )
+        Integer memId = loginUser.getMemid();
+        model.addAttribute("memId", memId);
         // 1) 從 Service 查詢資料 (假設會員ID固定測試 = 2，或動態取得)
-        List<CourtOrderVO> orders = courtOrderSvc.findOrdersWithDetailsByMemberId(2);
+        List<CourtOrderVO> orders = courtOrderSvc.findOrdersWithDetailsByMemberId(memId);
 
         // 2) 放入 model
         model.addAttribute("orders", orders);
+        
 
         // 3) 返回 thymeleaf 模板名稱(不含 .html 副檔名)
-        return "/back-end/member/appointmentRecord";
+        return "/back-end/member/AppointmentRecord";
     }
 }
 
--- src/main/java/com/smashspot/interceptor/WebConfig.java
+++ src/main/java/com/smashspot/interceptor/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginInterceptor())
 
 	        .addPathPatterns("/member/basic-info","/client/memProductList",
-	        				 "/client/addProduct","/reservation/week","/client/orders/DPstep1/{proid}")
+	        				 "/client/addProduct","/reservation/week","/client/orders/DPstep1/{proid}","member/appointment-records", "/court-order/**")
 	        .excludePathPatterns("/member/login");
 
     }
